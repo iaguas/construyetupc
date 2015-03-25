c62a75957343d1d166e5b2df8352bd3a197c54fa
AlvaroLarumbe <biobiotm@gmail.com>
2015-03-25 19:12:15 +0100

    Ahora se pueden añadir documentos a una colección y crear una colección (innecesario?).

--- models/model.php
+++ models/model.php
@@ -13,22 +13,65 @@
  */
 class DBHelper {
     // Datos de conexión
-    private $mongoIp = '127.0.0.1';
-    private $mongoPort = 27017;
     private $mongoDbName = 'construyetupc';
+    private $db;
 
     /**
      * Método de conexión a la BD
      * Conexión local
      */
-    public function mConnectMongo() {
+    public function __construct() {
         $mon = new MongoClient();
         $db = $mon->selectDB($this->mongoDbName);
-        echo 'Test conexión MongoDB: ';
-        print($db);
+        $this->db = $db;
     }
+
+    /**
+     * Crea una colección en la BD.
+     *
+     * @param $name nombre de la colección.
+     * @return mixed
+     */
+    public function mCreateCollection($name) {
+        return $this->db->createCollection($name);
+    }
+
+    /**
+     * Inserta un documento en una colección.
+     *
+     * @param $document documento a insertar.
+     * @param $collection colección donde insertarlo.
+     */
+    public function mInsertDocument($document, $collection) {
+        $col = $this->db->$collection;
+        $col->insert($document);
+    }
+
+    /**
+     * NO USAR!! En construcción...
+     */
+    public function mGetEmailsLanding() {
+        $col = $this->db->emails_landing;
+        $cursor = $col->find();
+
+        foreach($cursor as $doc) {
+            // Insertar $doc en un array, así hasta que acabe el foreach y returnarlo!
+        }
+    }
+
 }
 
-/*$db = new DBHelper();
-$db->mConnectMongo();*/
+/**
+ * ¡¡¡¡¡¡¡¡ MUNARRIZ !!!!!!!!
+ *
+ * Básate en este código para hacer lo del correo de la landing page.
+ */
+//$db = new DBHelper();
+//$db->mCreateCollection('emails_landing');
+
+/*$doc1 = array(
+    "email" => "test1@test.com"
+);
+
+$db->mInsertDocument($doc1, 'emails_landing');*/
 ?>
