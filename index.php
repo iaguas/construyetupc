914f424941651e8c058b490311286509716af34b
AlvaroLarumbe <biobiotm@gmail.com>
2015-03-29 19:09:15 +0200

    Arreglada la indentacion de todo el codigo con tTabulaciones de 4 espacios.

--- index.php
+++ index.php
@@ -1,81 +1,79 @@
 <?php
 /**
-*	Fichero: index.php
-* 	Descripcion: Archivo que contiene todo lo relacionado con el controlador del patrón MVC.
-*/
-	
-	require_once("models/model.php");
-    require_once("view.php");
+ *	Fichero: index.php
+ * 	Descripcion: Archivo que contiene todo lo relacionado con el controlador del patrón MVC.
+ */
 
-	/**
-	 * Control de la acción que se lleva a cabo.
-	 */
-	if (isset($_GET["action"])){
-		$action = $_GET["action"]; 
-	}	
-	else {
-		if (isset($_POST["action"])){
-			$action = $_POST["action"];
-		}
-		else {
-			$action = "landing";
-            // Descomentar cuando quitemos la Landing Page
-            //$action = "main";
-		}
-	}	
+require_once("models/model.php");
+require_once("view.php");
 
-	/**
-	 * Control de la id.
-	 */
-	if (isset($_GET["id"])){
-		$id = $_GET["id"]; 
-	}	
-	else {
-		if (isset($_POST["id"])) {
-			$id = $_POST["id"];
-		}
-		else {
-			$id = 1;
-		}
-	}
+/**
+ * Control de la acción que se lleva a cabo.
+ */
+if (isset($_GET["action"])){
+    $action = $_GET["action"];
+}
+else {
+    if (isset($_POST["action"])){
+        $action = $_POST["action"];
+    }
+    else {
+        $action = "landing";
+        // Descomentar cuando quitemos la Landing Page
+        //$action = "main";
+    }
+}
 
-	/**
-	 * Cargamos la página principal
-	 */
+/**
+ * Control de la id.
+ */
+if (isset($_GET["id"])){
+    $id = $_GET["id"];
+}
+else {
+    if (isset($_POST["id"])) {
+        $id = $_POST["id"];
+    }
+    else {
+        $id = 1;
+    }
+}
 
-	switch($action) {
+/**
+ * Cargamos la página principal
+ */
 
-        // Landing page
-        case 'landing':
-            switch ($id){
-                case 1:
-                	vLandingPage();
-                	break;
-            	case 2:
-                	vQuienesSomos();
-                	break;
-                }
-            break;
+switch($action) {
+    // Landing page
+    case 'landing':
+        switch ($id){
+            case 1:
+                vLandingPage();
+                break;
+            case 2:
+                vQuienesSomos();
+                break;
+        }
+        break;
 
-        // Main page
-        case 'main':
-            switch ($id){
-                case 1:
-                    vMainPage();
-                    break;
-                }
-            break;
+    // Main page
+    case 'main':
+        switch ($id){
+            case 1:
+                vMainPage();
+                break;
+        }
+        break;
 
-		// Selección de tipo de componente
-        case 'partList':
-            switch ($id){
-                case 1:
-                    vShowPartList();
-                    break;
-				case 2:
-					vShowComponentSelection();
-					break;
-            }
-            break;
-	
-	}
+    // Selección de tipo de componente
+    case 'partList':
+        switch ($id){
+            case 1:
+                vShowPartList();
+                break;
+            case 2:
+                vShowComponentSelection();
+                break;
+        }
+        break;
+}
--- models/model.php
+++ models/model.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 /**
-*	Fichero: model.php
-* 	Descripcion: Archivo que contiene todo lo relacionado con el modelo del patrón MVC.
-*		El nombre de las funciones empiezan por la letra "m" (hace referencia al modelo para que se más sencillo localizarlo).
-*		Después de dicha letra, las palabras empiezan con mayúsculas.
-*/
+ *	Fichero: model.php
+ * 	Descripcion: Archivo que contiene todo lo relacionado con el modelo del patrón MVC.
+ *		El nombre de las funciones empiezan por la letra "m" (hace referencia al modelo para que se más sencillo localizarlo).
+ *		Después de dicha letra, las palabras empiezan con mayúsculas.
+ */
 
 // Clase de encapsulación para MongoDB
 require_once 'DBHelper.php';
