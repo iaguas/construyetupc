7935b6741eeae483f08440617483500fdb3bba46
AlvaroLarumbe <biobiotm@gmail.com>
2015-03-29 21:21:41 +0200

    Método para obtener un documento a partir del nombre de la colección y el ID.

--- models/IDBHelper.php
+++ models/IDBHelper.php
@@ -6,6 +6,30 @@
  */
 interface IDBHelper {
 
+    /***********************/
+    /* Métodos de consulta */
+    /***********************/
+
+    /**
+     * Obtiene la lista de correos recibidos en la landing page.
+     *
+     * @return mixed array de correos.
+     */
+    public function mGetEmailsLanding();
+
+    /**
+     * Obtiene un sólo documento de una colección con un ID.
+     *
+     * @param $colName nombre de la colección.
+     * @param $id identificador del documento (string).
+     * @return array|null array que contiene el documento.
+     */
+    public function mGetDocumentByColAndId($colName, $id);
+
+    /************************/
+    /* Métodos de inserción */
+    /************************/
+
     /**
      * Crea una colección en la BD.
      *
@@ -23,17 +47,22 @@ interface IDBHelper {
     public function mInsertDocument($document, $collection);
 
     /**
-     * Obtiene la lista de correos recibidos en la landing page.
-     *
-     * @return mixed array de correos.
-     */
-    public function mGetEmailsLanding();
-
-    /**
      * Inserta un correo en la colección "emails_landing".
      *
      * @param string email a insertar.
      */
     public function mInsertEmailLanding($email);
+
+    /****************************/
+    /* Métodos de actualización */
+    /****************************/
+
+
+
+    /**********************/
+    /* Métodos de borrado */
+    /**********************/
+
+
 
 }
