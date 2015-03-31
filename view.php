09bedafbb376456c6095d5d214de202899514680
Kevin Marques <kevin.k93@hotmail.com>
2015-03-31 15:12:32 +0200

    Diseño del listado de componentes
    El listado se carga de forma dinámica.

--- view.php
+++ view.php
@@ -35,6 +35,33 @@ function vMainPage() {
  */
 function vShowPartList() {
     $page = file_get_contents("views/partlist.html");
+
+    // TODO: obtener categorías de la BD
+    $categories = array(
+        array('cpu', 'CPU', 'assets/img/hard-icons/cpu.png'),
+        array('gpu', 'GPU', 'assets/img/hard-icons/gpu.png'),
+        array('cpu-cooler', 'Ventilador CPU', 'assets/img/hard-icons/cpu-cooler.png'),
+        array('motherboard', 'Placa base','assets/img/hard-icons/motherboard.png'),
+        array('memory', 'Memoria RAM','assets/img/hard-icons/memory.png'),
+        array('power-supply', 'Fuente de alimentación','assets/img/hard-icons/power-supply.png'),
+        array('case', 'Torre/Caja','assets/img/hard-icons/case.png'),
+        array('optical-drive', 'Unidad óptica','assets/img/hard-icons/optical-drive.png'),
+        array('storage', 'Almacenamiento','assets/img/hard-icons/storage.png'),
+        array('monitor', 'Monitor','assets/img/hard-icons/monitor.png')
+    );
+
+    $dhtml = '';
+    foreach($categories as $category){
+        $dhtml .= "<tr>";
+        $dhtml .= "<td class='col-md-1 vert-align'>" . $category[1] . "</td>";
+        $dhtml .= "<td class='col-md-3 vert-align'><button type='button' class='btn btn-default' onclick='window.location.href=\"index.php?action=partList&id=2&part=\"'" . $category[0] . "><span class='glyphicon glyphicon-search'></span> Elegir " . $category[1] . "</button></td>";
+        $dhtml .= "<td class='col-md-2 vert-align'></td>";
+        $dhtml .= "<td class='col-md-1 vert-align'></td>";
+        $dhtml .= "</tr>";
+    }
+
+    $page = str_replace("{{component-list}}", $dhtml, $page);
+
     echo $page;
 }
 
