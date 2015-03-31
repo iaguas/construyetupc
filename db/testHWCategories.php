53bfe92aa608adb392949ea847cc70a0a2382f6c
AlvaroLarumbe <biobiotm@gmail.com>
2015-03-31 18:03:22 +0200

    Arreglado el método de añadir emails y creados nuevos métodos para insertar un archivo JSON en BD, vaciar una colección y eliminar elementos concretos de la misma. Añadida también la carpeta "db" donde se almacenarán archivos de datos de la BD y snippets de código de la BD.

---
+++ db/testHWCategories.php
@@ -0,0 +1,22 @@
+<?php
+require_once '../models/model.php';
+
+header('Content-Type: text/html; charset=utf-8');
+
+// Mejor no descomentar
+
+//$db->mRemoveAllInCollection('hardware_categories');
+
+//$json = file_get_contents('../db/hardware_categories.json');
+//$db->mInsertJson($json, 'hardware_categories');
+
+// Listar categorías
+$res = $db->mGetHardwareCategories();
+
+foreach($res as $doc) {
+    echo $doc['_id'] . '<br>';
+    echo $doc['name'] . '<br>';
+    echo $doc['spanishName'] . '<br>';
+    echo $doc['img'] . '<br>';
+    echo '<br>';
+}
