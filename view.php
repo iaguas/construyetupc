f6eab8d83e1eef5306252655104435b05f9dd176
Iñigo Aguas <aguas.71332@e.unavarra.es>
2015-05-31 02:57:55 +0200

    Implementada la muestra en detalle de la categoría 'cpu-cooler'
    Se modifica la página html para incluirla en el formato actual que solo
    dispone del contenido de la página y se personaliza para la categoría
    indicada.

--- view.php
+++ view.php
@@ -400,29 +400,41 @@ function vShowDetailedPartModel($part, $id){
                 $page = str_replace('{{image-description}}', $modelName,$page);
             }
             break;
+        //////////////////////////////  CPU-COOLER  //////////////////////////////  
         case 'cpu-cooler':
-            $page = file_get_contents("views/detailedComponents/cpu-cooler.html");
+            $page = vFillTemplatePublic('views/detailedComponents/cpu-cooler.html');
             $dhtml = '';
             $model = $id;
-            // TODO: Fijar nombre correcto de la base de datos
-            $modelName = $db->mGetCompName($model,'cpu-coolers');
+            $modelName = $db->mGetCompName($model,'cpucoolers');
 
             // TODO: Obtener lista de todas las tiendas que tienen el modelo solicitado y sus precios
-            $coolers = $db->mGetCompPrices($model,'cpu-coolers');
-            // TODO: Obtener especificaciones técnicas del modelo solicitado
-            foreach ($coolers as $key => $partItem){
+            $processors = $db->mGetCompPrices($model,'cpucoolers');
+            $properties = $db->mGetCompProperties($model, 'cpucoolers');
+            foreach ($processors as $key => $partItem){
                 $total = @$partItem['price'] + @$partItem['delivery-fare'];
                 $dhtml .= "<tr>";
-                $dhtml .= "<input id='product-id' type='hidden' value='" . $key . "'>";
-                $dhtml .= "<td class='col-md-3 vert-align'>" . @$partItem['provider'] . "</td>";
-                $dhtml .= "<td class='col-md-1 vert-align'>" . @$partItem['price'] . "</td>";
-                $dhtml .= "<td class='col-md-1 vert-align'>" . @$partItem['delivery-fare'] . "</td>";
-                $dhtml .= "<td class='col-md-1 vert-align'>" . (string)$total  . "</td>";
-                $dhtml .= "<td class='col-md-1 vert-align'><button type='button' onclick='window.location.href=\"partList/select/cpu-cooler/" . $key ."\"'>Añadir</button></td>";
+                $dhtml .= "<input id='product-id' type='hidden' value='" . $properties['pn'] . "'>";
+                $dhtml .= "<td id='product-vendor' class='col-md-3 vert-align'>" . @$partItem['provider'] . "</td>";
+                $dhtml .= "<td id='product-price' class='col-md-1 vert-align'>" . @$partItem['price']. "€" . "</td>";
+                $dhtml .= "<td class='col-md-1 vert-align'>" . @$partItem['delivery-fare']. "€" . "</td>";
+                $dhtml .= "<td class='col-md-1 vert-align'>" . $total  . "€". "</td>";
+                $dhtml .= "<td class='col-md-1 vert-align'><button id='add-product' type='button'>Añadir</button></td>";
                 $dhtml .= "</tr>";
             }
+            // Insertar las especficificaciones técnicas.
             $page = str_replace('{{component-name}}', $modelName, $page);
-            $page = str_replace('{{vendor-cpu-cooler-list}}', $dhtml, $page);
+            $page = str_replace('{{vendor-processor-list}}', $dhtml, $page);
+            $page = str_replace('{{rpm}}', $properties['rpm'],$page);
+            $page = str_replace('{{size}}', $properties['size'],$page);
+            // Rellenar si es sin imagen de forma adecuada.
+            if ($properties['img']=="") {
+                $page = str_replace('{{image-url}}', "./assets/img/no-image150x150.png",$page);
+                $page = str_replace('{{image-propieties}}', "No existe imagen",$page);
+            }
+            else{
+                $page = str_replace('{{image-url}}', $properties['img'],$page);
+                $page = str_replace('{{image-propieties}}', $modelName,$page);
+            }
             break;
         case 'memory':
             $page = file_get_contents("views/detailedComponents/memory.html");
