36bbb33b168a7906051f66042e43396189913f62
Iñigo Aguas <aguas.71332@e.unavarra.es>
2015-05-31 03:04:20 +0200

    Implementada la muestra en detalle de la categoría 'optical-drive'
    Se modifica la página html para incluirla en el formato actual que solo
    dispone del contenido de la página y se personaliza para la categoría
    indicada.

--- view.php
+++ view.php
@@ -601,15 +601,42 @@ function vShowDetailedPartModel($part, $id){
                 $page = str_replace('{{image-propieties}}', $modelName,$page);
             }
             break;
+
+        //////////////////////////////  OPTICAL DRIVE  //////////////////////////////  
+        case 'optical-drive':
+            $page = vFillTemplatePublic('views/detailedComponents/optical-drive.html');
             $dhtml = '';
             $model = $id;
+            $modelName = $db->mGetCompName($model, str_replace('-', '', $part).'s');
 
             // TODO: Obtener lista de todas las tiendas que tienen el modelo solicitado y sus precios
+            $processors = $db->mGetCompPrices($model, str_replace('-', '', $part).'s');
+            $properties = $db->mGetCompProperties($model, str_replace('-', '', $part).'s');
+
+            foreach ($processors as $key => $partItem){
                 $total = @$partItem['price'] + @$partItem['delivery-fare'];
                 $dhtml .= "<tr>";
+                $dhtml .= "<input id='product-id' type='hidden' value='" . $properties['pn'] . "'>";
+                $dhtml .= "<td id='product-vendor' class='col-md-3 vert-align'>" . @$partItem['provider'] . "</td>";
+                $dhtml .= "<td id='product-price' class='col-md-1 vert-align'>" . @$partItem['price']. "€" . "</td>";
+                $dhtml .= "<td class='col-md-1 vert-align'>" . @$partItem['delivery-fare']. "€" . "</td>";
+                $dhtml .= "<td class='col-md-1 vert-align'>" . $total  . "€". "</td>";
+                $dhtml .= "<td class='col-md-1 vert-align'><button id='add-product' type='button'>Añadir</button></td>";
                 $dhtml .= "</tr>";
             }
+            // Insertar las especficificaciones técnicas.
             $page = str_replace('{{component-name}}', $modelName, $page);
+            $page = str_replace('{{vendor-processor-list}}', $dhtml, $page);
+            // Esta categoría no tiene especificaciones técnicas
+            // Rellenar si es sin imagen de forma adecuada.
+            if ($properties['img']=="") {
+                $page = str_replace('{{image-url}}', "./assets/img/no-image150x150.png",$page);
+                $page = str_replace('{{image-propieties}}', "No existe imagen",$page);
+            }
+            else{
+                $page = str_replace('{{image-url}}', $properties['img'],$page);
+                $page = str_replace('{{image-propieties}}', $modelName,$page);
+            }
             break;
             $dhtml = '';
             $model = $id;
@@ -622,29 +649,15 @@ function vShowDetailedPartModel($part, $id){
             // Insertar las especficificaciones técnicas.
             $page = str_replace('{{component-name}}', $modelName, $page);
             break;
-        case 'optical-drive':
-            $page = file_get_contents("views/detailedComponents/optical-drive.html");
             $dhtml = '';
             $model = $id;
-            // TODO: Fijar nombre correcto de la base de datos
-            $modelName = $db->mGetCompName($model,'optical-drives');
 
             // TODO: Obtener lista de todas las tiendas que tienen el modelo solicitado y sus precios
-            $opticaldrives = $db->mGetCompPrices($model,'optical-drives');
-            // TODO: Obtener especificaciones técnicas del modelo solicitado
-            foreach ($opticaldrives as $key => $partItem){
                 $total = @$partItem['price'] + @$partItem['delivery-fare'];
                 $dhtml .= "<tr>";
-                $dhtml .= "<input id='product-id' type='hidden' value='" . $key . "'>";
-                $dhtml .= "<td class='col-md-3 vert-align'>" . @$partItem['provider'] . "</td>";
-                $dhtml .= "<td class='col-md-1 vert-align'>" . @$partItem['price'] . "</td>";
-                $dhtml .= "<td class='col-md-1 vert-align'>" . @$partItem['delivery-fare'] . "</td>";
-                $dhtml .= "<td class='col-md-1 vert-align'>" . (string)$total  . "</td>";
-                $dhtml .= "<td class='col-md-1 vert-align'><button type='button' onclick='window.location.href=\"partList/select/optical-drive/" . $key ."\"'>Añadir</button></td>";
                 $dhtml .= "</tr>";
             }
             $page = str_replace('{{component-name}}', $modelName, $page);
-            $page = str_replace('{{vendor-optical-drive-list}}', $dhtml, $page);
             break;
     }
     echo $page;
